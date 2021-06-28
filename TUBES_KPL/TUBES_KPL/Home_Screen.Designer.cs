97ed1aa2b8c5670d7ada43a3c677997d5e7767fc
asrawinata <asra.winata@gmail.com>
2021-06-28 11:03:03 +0700

    Adding "Home - E Parking"

---
+++ TUBES_KPL/TUBES_KPL/Home_Screen.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace TUBES_KPL
+{
+    partial class Home_Screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.parkButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // parkButton
+            // 
+            this.parkButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.parkButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.parkButton.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.parkButton.ForeColor = System.Drawing.Color.White;
+            this.parkButton.Location = new System.Drawing.Point(81, 245);
+            this.parkButton.Name = "parkButton";
+            this.parkButton.Size = new System.Drawing.Size(234, 51);
+            this.parkButton.TabIndex = 0;
+            this.parkButton.Text = "Park";
+            this.parkButton.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.button2.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(81, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Payment";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkRed;
+            this.button3.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(81, 555);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(234, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Report";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-2, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 113);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "e - Parking";
+            // 
+            // Home_Screen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 857);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.parkButton);
+            this.Name = "Home_Screen";
+            this.Text = "Home - E Parking";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button parkButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
