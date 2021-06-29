8f8b7853f5de9105c8107805aee01d59e6858d58
althoof <muhammadalthoof@gmail.com>
2021-06-29 14:15:37 +0700

    Payment

--- TUBES_KPL/TUBES_KPL/Payment.cs
+++ TUBES_KPL/TUBES_KPL/Payment.cs
@@ -16,5 +16,20 @@ namespace TUBES_KPL
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
